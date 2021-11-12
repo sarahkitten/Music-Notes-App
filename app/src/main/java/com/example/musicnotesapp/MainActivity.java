b41065c1f1abc6e2185174ce6064a470c5676e4d
Sarah Kitten <59421998+sarahkitten@users.noreply.github.com>
2021-11-12 15:33:43 -0800

    Finished basic drawing functionality

--- app/src/main/java/com/example/musicnotesapp/MainActivity.java
+++ app/src/main/java/com/example/musicnotesapp/MainActivity.java
@@ -1,14 +1,220 @@
+// MainActivity
+// By Sarah Kitten based on https://youtu.be/H9CTVyu3Bi8
+// View for options menu to change color/line width, clear, or save
+
 package com.example.musicnotesapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import view.PikassoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PikassoView pikassoView;
+    private AlertDialog.Builder currentAlertDialog;
+    private ImageView widthImageView;
+    private AlertDialog dialogLineWidth;
+    private AlertDialog colorDialog;
+
+    private SeekBar alphaSeekBar;
+    private SeekBar redSeekBar;
+    private SeekBar greenSeekBar;
+    private SeekBar blueSeekBar;
+    private View colorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // setup on app open
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pikassoView = findViewById(R.id.view);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // show options menu
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.clearId:  // invoke clear
+                pikassoView.clear();
+                break;
+
+            case R.id.saveid:  // invoke save
+                pikassoView.saveToInternalStorage();
+                break;
+
+            case R.id.colorid:  // invoke color select
+                showColorDialog();
+                break;
+
+            case R.id.lineWidth:  // invoke line width select
+                showLineWidthDialog();
+                break;
+
+            case R.id.eraseId:  // invoke erase
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    void showColorDialog() {
+        currentAlertDialog = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.color_dialog, null);  // show dialog
+        // get ARGB seekbars
+        alphaSeekBar = view.findViewById(R.id.alphaSeekBar);
+        redSeekBar = view.findViewById(R.id.redSeekBar);
+        greenSeekBar = view.findViewById(R.id.greenSeekBar);
+        blueSeekBar = view.findViewById(R.id.blueSeekBar);
+        colorView = view.findViewById(R.id.colorView);
+
+        // register SeekBar event listeners
+        alphaSeekBar.setOnSeekBarChangeListener(colorSeekBarChanged);
+        redSeekBar.setOnSeekBarChangeListener(colorSeekBarChanged);
+        greenSeekBar.setOnSeekBarChangeListener(colorSeekBarChanged);
+        blueSeekBar.setOnSeekBarChangeListener(colorSeekBarChanged);
+
+        int color = pikassoView.getDrawingColor();
+        alphaSeekBar.setProgress(Color.alpha(color));
+        redSeekBar.setProgress(Color.red(color));
+        greenSeekBar.setProgress(Color.green(color));
+        blueSeekBar.setProgress(Color.blue(color));
+
+        Button setColorButton = view.findViewById(R.id.setColorButton);
+        setColorButton.setOnClickListener(new View.OnClickListener() {
+            // set drawing color to the selected ARGB values
+            @Override
+            public void onClick(View v) {
+                pikassoView.setDrawingColor(Color.argb(
+                        alphaSeekBar.getProgress(),
+                        redSeekBar.getProgress(),
+                        greenSeekBar.getProgress(),
+                        blueSeekBar.getProgress()
+                ));
+
+                colorDialog.dismiss();  // dismiss dialog
+                currentAlertDialog = null;
+            }
+        });
+
+        currentAlertDialog.setView(view);
+        currentAlertDialog.setTitle("Choose Color");
+        colorDialog = currentAlertDialog.create();
+        colorDialog.show();
+    }
+
+    void showLineWidthDialog() {
+        // show line width dialog with line width seekbar and preview image
+
+        currentAlertDialog = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.width_dialog, null);
+        SeekBar widthSeekbar = view.findViewById(R.id.widthSeekBar);
+        Button setLineWidthButton = view.findViewById(R.id.widthDialogButton);
+        widthImageView = view.findViewById(R.id.imageViewId);
+        setLineWidthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // set line width and dismiss dialog
+                pikassoView.setLineWidth(widthSeekbar.getProgress());
+                dialogLineWidth.dismiss();
+                currentAlertDialog = null;
+            }
+        });
+
+        widthSeekbar.setOnSeekBarChangeListener(widthSeekbarChange);
+        widthSeekbar.setProgress(pikassoView.getLineWidth());
+
+        currentAlertDialog.setView(view);
+        dialogLineWidth = currentAlertDialog.create();
+        dialogLineWidth.setTitle("Set line Width");
+
+        dialogLineWidth.show();
+    }
+
+    private SeekBar.OnSeekBarChangeListener colorSeekBarChanged = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            pikassoView.setBackgroundColor(Color.argb(
+                    alphaSeekBar.getProgress(),
+                    redSeekBar.getProgress(),
+                    greenSeekBar.getProgress(),
+                    blueSeekBar.getProgress()
+            ));
+
+            // display the current color
+            colorView.setBackgroundColor(Color.argb(
+                    alphaSeekBar.getProgress(),
+                    redSeekBar.getProgress(),
+                    greenSeekBar.getProgress(),
+                    blueSeekBar.getProgress()
+            ));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private SeekBar.OnSeekBarChangeListener widthSeekbarChange = new SeekBar.OnSeekBarChangeListener() {
+        Bitmap bitmap = Bitmap.createBitmap(400, 100, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            // set stroke width and preview image based on seekbar progress
+
+            Paint p = new Paint();
+            p.setColor(pikassoView.getDrawingColor());
+            p.setStrokeCap(Paint.Cap.ROUND);
+            p.setStrokeWidth(progress);
+
+            bitmap.eraseColor(Color.WHITE);
+            canvas.drawLine(30, 50, 370, 50, p);
+            widthImageView.setImageBitmap(bitmap);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
 }
