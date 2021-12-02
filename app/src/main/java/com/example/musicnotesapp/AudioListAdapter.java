ebfc80f7a208e458fcb100d859eed28dd3b795c1
Wally Wallaby <asummer8@uoregon.edu>
2021-12-01 16:28:54 -0800

    Kale and Alex added delete functionality

--- app/src/main/java/com/example/musicnotesapp/AudioListAdapter.java
+++ app/src/main/java/com/example/musicnotesapp/AudioListAdapter.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +20,8 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
     private TimeAgo timeAgo;
 
     private final onItemListClick onItemListClick;
+
+    private int delete_flag = 0; // set delete file flag automaticly to not delete file
 
     public AudioListAdapter(List<File> allFiles, onItemListClick onItemListClick){
         this.allFiles = allFiles; // assign allFiles var from FileListFragment to this
@@ -37,13 +40,18 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
     public void onBindViewHolder(@NonNull AudioViewHolder holder, int position) {
         holder.list_title.setText(allFiles.get(position).getName());
         Log.d("Image:", holder.list_title.getText().toString());
+
+        // assign delete button
+        holder.delete_btn = holder.itemView.findViewById(R.id.delete_button);
+
         if(holder.list_title.getText().toString().endsWith("jpg")) {
             holder.itemView.findViewById(R.id.list_image_view).setVisibility(View.INVISIBLE);
             holder.list_image = holder.itemView.findViewById(R.id.list_image_view2);
             }
-        else
+        else {
             holder.itemView.findViewById(R.id.list_image_view2).setVisibility(View.INVISIBLE);
             holder.list_image = holder.itemView.findViewById(R.id.list_image_view);
+        }
 
         holder.list_date.setText(timeAgo.getTimeAgo(allFiles.get(position).lastModified()));
 
@@ -56,9 +64,12 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
 
     public class AudioViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private ImageView list_image;
+        private ImageView list_image; // to play a recording
+        private ImageView list_image2; // to pull up a saved image
         private final TextView list_title;
         private final TextView list_date;
+
+        private ImageButton delete_btn;
 
         public AudioViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,19 +77,28 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
             list_title = itemView.findViewById(R.id.list_title);
             list_date = itemView.findViewById(R.id.list_date);
             list_image = itemView.findViewById(R.id.list_image_view);
+            list_image2 = itemView.findViewById(R.id.list_image_view2);
 
-            itemView.setOnClickListener(this);
+            delete_btn = itemView.findViewById(R.id.delete_button);
 
+            list_image.setOnClickListener(this); // the play recording
+            list_image2.setOnClickListener(this); // pull up image
+            delete_btn.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            onItemListClick.onClickListener(allFiles.get(getAdapterPosition()), getAdapterPosition());
+
+            if(v.getId() == R.id.delete_button){ // if delete button pressed
+                onItemListClick.onClickListener(allFiles.get(getAdapterPosition()), getAdapterPosition(), true);
+            }else{ // if anything else pressed
+                onItemListClick.onClickListener(allFiles.get(getAdapterPosition()), getAdapterPosition(), false);
+            }
         }
     }
 
     public interface onItemListClick {
-        void onClickListener(File file, int position);
+        void onClickListener(File file, int position, boolean delete_flag);
     }
 
 }
