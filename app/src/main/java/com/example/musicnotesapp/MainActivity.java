08ddf6ceae0ea0845c4beb2aad49a9e15ba2074f
Wally Wallaby <asummer8@uoregon.edu>
2021-12-02 21:23:10 -0800

    commented Main Activity

--- app/src/main/java/com/example/musicnotesapp/MainActivity.java
+++ app/src/main/java/com/example/musicnotesapp/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+Source Code for the initialization used in the Audio Recording, CIS422 FA21
+Author(s): Alex Summers
+Last Edited: 12/2/21
+Sources: N/A
+*/
+
 package com.example.musicnotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // create saved
+        super.onCreate(savedInstanceState); // create parent save instance
         setContentView(R.layout.activity_main); // give navigation rights to nav graph
     }
 
     @Override
     public void onBackPressed(){
-        //super.onBackPressed();
+        // make this function do nothing so the android system back button does
+        // nothing because it brought bugs into our file display where if navigating
+        // through fragments it would duplicate files in the file display
     }
 
 }
